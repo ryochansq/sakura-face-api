855001441ffde571b7f9399ccae7bbb22c25ae84
ryochansq <nicestsnsq@gmail.com>
2021-06-03 01:35:04 +0900

    test body

--- handler.ts
+++ handler.ts
@@ -11,20 +11,12 @@ const client = new FaceClient(credentials, endpoint);
 
 export const detect: APIGatewayProxyHandler = async (event, _context) => {
   await new Promise((resolve)=>resolve([event, _context]));
-  const res = await client.face.detectWithStream(event.body)
-  .catch((err)=> ({
-    statusCode: 500,
-    body: JSON.stringify({
-      message: 'detect',
-      err,
-    }, null, 2),
-
-  }));
+  // const res = await client.face.detectWithStream(event.body).catch((err)=> err);
   return {
     statusCode: 200,
     body: JSON.stringify({
       message: 'detect',
-      res,
+      img: event.body,
     }, null, 2),
   };
 }
@@ -33,10 +25,6 @@ export const findSimilar: APIGatewayProxyHandler = async (event, _context) => {
   await new Promise((resolve)=>resolve('hoge'));
   return {
     statusCode: 200,
-    body: JSON.stringify({
-      message: 'find similar',
-      event,
-      _context,
-    }, null, 2),
+    body: event.body,
   };
 }
