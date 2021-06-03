1d7e2044a1f16c0af1cc8173112ecdccff5db86b
ryochansq <nicestsnsq@gmail.com>
2021-06-04 00:05:09 +0900

    return error at detect

--- handler.ts
+++ handler.ts
@@ -3,6 +3,16 @@ import 'source-map-support/register';
 import { ApiKeyCredentials } from '@azure/ms-rest-js';
 import { FaceClient } from '@azure/cognitiveservices-face';
 
+interface IError {
+  statusCode: number;
+  body: {
+    error: {
+      code: string;
+      message: string;
+    }
+  }
+}
+
 const key = process.env.AZURE_API_KEY;
 const endpoint = 'https://sakura-face-api.cognitiveservices.azure.com/';
 
@@ -11,13 +21,21 @@ const client = new FaceClient(credentials, endpoint);
 
 export const detect: APIGatewayProxyHandler = async (event) => {
   const image = Buffer.from(event.body, 'base64');
-  const res = await client.face.detectWithStream(image);
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      res,
-    }, null, 2),
-  };
+  try {
+    const res = await client.face.detectWithStream(image);
+    return {
+      statusCode: 200,
+      body: JSON.stringify({
+        res,
+      }, null, 2),
+    };
+  } catch (e) {
+    const error = e as IError
+    return {
+      statusCode: error.statusCode,
+      body: JSON.stringify(error.body, null, 2),
+    };
+  }
 }
 
 export const findSimilar: APIGatewayProxyHandler = async (event) => {
